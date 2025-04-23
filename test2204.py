0684810a0e5bf8b4d8389b78b676f6679b50165e
Ragul <sarwanragul65@gmail.com>
2025-04-23 13:06:11 +0530

    initial commit

---
+++ test2204.py
@@ -0,0 +1,10 @@
+#E-commerce Product Name Project
+
+brand_name=input("what is your brand name:")
+
+model_name=input("what is your model name:")
+
+product_name = brand_name+ " " + model_name
+
+print(product_name)
+
