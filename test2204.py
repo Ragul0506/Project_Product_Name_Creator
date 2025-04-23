898a2ff4813ef21be741b851af231108ab3b7158
Ragul0506 <sarwanragul65@gmail.com>
2025-04-23 14:30:44 +0530

    Update test2204.py

--- test2204.py
+++ test2204.py
@@ -1,9 +1,9 @@
 #E-commerce Product Name Project
-
+#input Fun user gives brand name
 brand_name=input("what is your brand name:")
-
+#input Fun user gives model name 
 model_name=input("what is your model name:")
-
+# product name is a varibel for stored combined name
 product_name = brand_name+ " " + model_name
 
 print(product_name)
